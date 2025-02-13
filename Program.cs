f0f4490be42eed117b4d3f162aff9111b5f35541
manumanoj2003 <nishanth121640@gmail.com>
2025-02-13 09:14:06 +0530

    version1

---
+++ Program.cs
@@ -0,0 +1,118 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace Day1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] myString = new string[4];
+            //myString[0] = "how";
+            //myString[1] = "are";
+            //myString[2] = "you";
+            //myString[3] = "?";
+            //for(int i=0;i<myString.Length;i++)
+            //{
+            //    Console.WriteLine(myString[i]); 
+            //}
+            //----------------------------
+            //int num1, num2, sum, prod, quot, rem;
+            //Console.WriteLine("enter 2 numbers");
+            //num1 =int.Parse(Console.ReadLine());
+            //num2= int.Parse(Console.ReadLine());
+            //sum = num1 + num2;
+            //prod = num1 * num2;
+            //quot = num1 / num2;
+            //rem = num1 % num2;
+            //Console.WriteLine("{0}+{1}={2}", num1, num2, sum);
+            //Console.WriteLine("{0}*{1}={2}", num1, num2, prod);
+            //Console.WriteLine("{0}/{1}={2}", num1, num2, quot);
+            //Console.WriteLine("{0}%{1}={2}", num1, num2, rem);
+            //----------------------------
+            //int number = 234567;
+            //int mag = 0;
+            //while(number>0)
+            //{
+            //    number = number / 10;
+            //    mag++;
+
+            //}
+            //Console.WriteLine("Magnitude=  "+ mag);
+            //-----------------------------------
+            //int number = 234;
+            //int digit;
+            //do
+            //{
+            //    digit = number % 10;
+            //    Console.Write(digit);
+            //    number = number / 10;
+            //} while (number>0);
+            //-------------------------------
+            //for(int i = 0;i<= 10;i++)
+            //    {
+            //    if(i%2!=0)
+            //    {
+            //        continue;
+            //    }
+            //    Console.WriteLine(i);
+            //}
+            //-------------------------------------
+            //int[] myArray = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            //int sum = 0;
+            //float avg = 0f;
+            //for (int i = 0; i < myArray.Length; i++)
+            //{
+            //    sum += myArray[i];
+            //}
+            //avg = sum / (float)myArray.Length;
+            //Console.WriteLine("sum={0},average={1}", sum, avg);
+            //-------------------------------------
+            //int querry;
+            //Console.WriteLine("enter a number");
+            //querry = int.Parse(Console.ReadLine());
+            //int[] myArray = new int[6] { 1, 2, 3, 4, 5, 6 };
+            //bool isFound = false;
+            //foreach (int item in myArray)
+            //{
+            //    if (item == querry)
+            //    {
+            //        isFound = true;
+            //        break;
+            //    }
+            //}
+            //if (isFound)
+            //{
+            //    Console.WriteLine(querry + " is found");
+            //}
+            //else
+            //{
+            //    Console.WriteLine(querry + "is not found");
+            //}
+            //-------------------------------
+            //    List<int> myList = new List<int>();
+            //    myList.Add(1);
+            //    myList.Add(10);
+            //    myList.Add(100);
+            //    myList.Add(1000);
+            //    Console.WriteLine(myList.Count);
+            //    myList.RemoveAt(2);
+            //    foreach(int item in myList)
+            //    {
+            //        Console.WriteLine(item);
+            //    }
+            //    Console.WriteLine(myList.Count);
+            //}
+            //-------------------------------------------
+            //Dictionary<int, string> D = new Dictionary<int, string>();
+            //D.Add(1, "Ready");
+            //D.Add(2, "For");
+            //D.Add(3, "Now");
+            //D.Add(4, "Tomorrow");
+            //foreach (KeyValuePair<int, string> item in D)
+            //{
+            //    Console.WriteLine("{0} <=> {1}", item.Key, item.Value);
+            //}
+            //------------------------------------------------
+        }
+    }
+}
